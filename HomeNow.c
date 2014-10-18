762230f35504e1fc2e057b6579752369e59793c0
slembcke <slembcke@gmail.com>
2014-10-18 15:45:13 -0500

    RFID initialization.

--- HomeNow.c
+++ HomeNow.c
@@ -1,6 +1,6 @@
-// This #include statement was automatically added by the Spark IDE.
 #include "Adafruit_SSD1306/Adafruit_SSD1306.h"
-
+#include "Adafruit_PN532.h"
+#include "notes.h"
 
 // If using software SPI (the default case):
 #define OLED_MOSI   2
@@ -22,101 +22,45 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-#define TEXT_WIDTH 21
+#define NFC_SCK  (A2)
+#define NFC_MOSI (A3)
+#define NFC_SS   (A4)
+#define NFC_MISO (A5)
+
+Adafruit_PN532 nfc(NFC_SCK, NFC_MISO, NFC_MOSI, NFC_SS);
+
+static void SetupNFC(void){
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Welcome("No NFC.");
+    while (1); // halt
+  }
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Welcome("NFC");
+}
+
+#define UID_LENGTH 4
+struct User {
+//  uint8_t uid[4];
+  char *name;
+  uint8_t uid[UID_LENGTH];
+};
+
+struct User Users[5] = {
+  {"Captain Faceman", {0x27, 0xBD, 0x77, 0x79}},
+  {"Jorge", {0x24, 0xB1, 0x41, 0xF4}},
+  {"The Stickler", {0x97, 0x2C, 0x11, 0xCB}},
+  {"Cardla", {0xC3, 0x71, 0x26, 0xD0}},
+  {"Baby Bertha", {0x47, 0x91, 0x33, 0x21}},
+};
 
 // State of our users:
 int jorge = 0;
-
-//music
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
-#define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
-#define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
 
 // notes in the melody:
 int melody[] = {
@@ -137,6 +81,7 @@ void setup()
 	// Configure the pins to be outputs
 	pinMode(LED_1, OUTPUT);
 	pinMode(LED_2, OUTPUT);
+	pinMode(D7, OUTPUT);
 	
 	// Initialize both the LEDs to be OFF
 	digitalWrite(LED_1, LOW);
@@ -153,9 +98,8 @@ void setup()
 	display.begin(SSD1306_SWITCHCAPVCC);
 	// init done
 	
-	Welcome("Captain Faceman");
-	
-	
+	// Setup the RFID reader.
+	SetupNFC();
 }
 
 
@@ -176,36 +120,50 @@ static void Welcome(char *name){
 }
 
 
-int debounce = true;
 int currentNote = -1;
+bool toggled = true;
 // This routine loops forever
 void loop()
 {
-	if(	digitalRead(TEMP_BUTTON) == HIGH && debounce){
-		debounce = false;
+	
+	if(	digitalRead(TEMP_BUTTON) == HIGH && toggled){
 		jorge = !jorge;
+		toggled = false;
+	    
+	    if(jorge){
+	        Welcome("JORGE AWAY");
+	    }else{
+	        Welcome("JORGE HOME");
+	    }
 	}else{
-		debounce = true;
+	    toggled = true;
 	}
 	
+	/*
 	if(currentNote != -1){
+	    
+	    char* str = "note: x";
+	    str[6] = currentNote;
+	    Welcome(str);
+	    
 		// to calculate the note duration, take one second
 		// divided by the note type.
 		//e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
 		int noteDuration = 1000/noteDurations[currentNote];
-		tone(8, melody[currentNote],noteDuration);
+		tone(D7, melody[currentNote],noteDuration);
 		
 		// to distinguish the notes, set a minimum time between them.
 		// the note's duration + 30% seems to work well:
 		int pauseBetweenNotes = noteDuration * 1.30;
 		delay(pauseBetweenNotes);
 		// stop the tone playing:
-		noTone(8);
+		noTone(D7);
 		currentNote = (currentNote+1);
 		if(currentNote > 8){
 			currentNote = -1;
 		}
 	}
+	*/
 	
 	// Nothing to do here
 }
